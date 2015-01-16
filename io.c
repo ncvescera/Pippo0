3fc330dd8aead4761b037b291ee56e3987d49c08
ncvescera <slayercandy@icloud.com>
2015-01-16 09:25:50 +0100

    ROBE

--- io.c
+++ io.c
@@ -3,5 +3,15 @@ int main(){
   printf("Asdasdasds");
   printf("Non devo essere qui");
   printf("Ora ti elimino");
+  printf("Ho fatto una modifica!");
+  printf("ASDASDASDASDSDSD");
+  //asdasdsadas
+
+  //yyyyyyyyyyyyyyyyyy
+  //ggggggggggggggggggggggggggg
+  //hhhhhhhhhhhhhhhh
+  
+  ///aqaaasdafsdggsgsgsg
+  system("pause");
   
 }
