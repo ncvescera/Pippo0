2a212727b54269f0dd74cea0e97dc32cc0ef8046
ncvescera <slayercandy@icloud.com>
2015-01-16 09:18:03 +0100

    ASD

--- io.c
+++ io.c
@@ -3,5 +3,11 @@ int main(){
   printf("Asdasdasds");
   printf("Non devo essere qui");
   printf("Ora ti elimino");
+  printf("Ho fatto una modifica!");
+  printf("ASDASDASDASDSDSD");
+  //asdasdsadas
+  //asdsdddddddddddddddddddddddd
+  //yyyyyyyyyyyyyyyyyy
+  system("pause");
   
 }
